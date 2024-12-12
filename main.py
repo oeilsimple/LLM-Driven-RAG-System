92f60b4d47c808702034dc5f0c9d3be2f4d6c1b5
oeilsimple <Oeilsimple@gmail.com>
2024-12-12 10:49:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-from langchain_groq import ChatGroq
 
-
-llm=ChatGroq(groq_api_key="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",model_name="Llama-3.2-90b-text-preview")
-
-if __name__=='__main__':
-    response=llm.invoke("suggest some indian dishes")
-    print(response)
